e647d8be81718838908a25683f21bd8ee101f433
Anthony Johnson <wontons713@Anthonys-MacBook-Pro.local>
2020-01-24 09:47:57 -0700

    Pulled in data from Star Wars API

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from "axios";
 
 const App = () => {
+  const [data, setData] = useState([]);
+    useEffect(() => {
+      axios.get("https://swapi.co/api/people/")
+      .then(res => {
+        console.log(res.data);
+        setData(res.data);
+      })
+      .catch(error => {
+        console.log("the data was not returned", error)
+      })
+    },[])
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
